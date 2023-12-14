50b2c30c164eb7243a52f857a600d8c3dc3cc109
Galaktro <galaktro@aliensbringchange.com>
2023-12-14 15:17:36 +0100

    auto commit

---
+++ finished-code-0301/car.py
@@ -0,0 +1,9 @@
+from pyglet.sprite import Sprite
+
+class Car:
+    def __init__(self, image, batch):
+        image.anchor_x = 25
+        image.anchor_y = 25
+        self.body = Sprite(image, batch=batch)
+        self.body.x, self.body.y = 480, 260
+        
