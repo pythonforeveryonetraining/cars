c264648a3f0317d26c194adae92e65d96f57a1ec
galaktro <galaktro>
2023-12-21 11:22:21 +0100

    auto commit

---
+++ finished-code-0405/hud.py
@@ -0,0 +1,12 @@
+from pyglet.text import Label
+
+class Hud:
+    def __init__(self, simulation_round, batch):
+        self.round_label = Label(f"Round: {simulation_round}", x=20, y=520, color=(0, 0, 0, 255), batch=batch)
+        self.population_label = Label(x=120, y=520, color=(0, 0, 0, 255), batch=batch)
+        self.speed_label = Label(x=280, y=520, color=(0, 0, 0, 255), batch=batch)
+        
+    def update(self, alive, population, speed):
+        self.population_label.text = f"Population: {alive}/{population}"
+        self.speed_label.text = f"Speed: {speed:.2f}"
+    
