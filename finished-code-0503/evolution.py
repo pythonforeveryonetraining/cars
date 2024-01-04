a526785cf6a83c64263f758c2a3b7cc0f0f6f5cc
Galaktro <galaktro@aliensbringchange.com>
2024-01-04 20:45:06 +0100

    auto commit

---
+++ finished-code-0503/evolution.py
@@ -0,0 +1,15 @@
+class Evolution:
+    def __init__(self, population_count, keep_count):
+        self.population_count = population_count
+        self.keep_count = keep_count
+        
+    def execute(self, rankable_chromosomes):
+        # selection
+        
+        # cross over
+        offspring = []
+        
+        # mutation
+        
+        assert len(offspring) == self.population_count, "Offspring count is not population_count"
+        return offspring
--- finished-code-0503/training.py
+++ finished-code-0503/training.py
@@ -1,7 +1,7 @@
 from canvas import Canvas
-import os
 from racetrack import Track
 from network import Network
+import os
 
 car_image_paths = [os.path.join("images", f"car{i}.png") for i in range(5)]
 canvas = Canvas(Track(1), car_image_paths)
@@ -14,8 +14,8 @@ networks = [Network(network_dimensions) for _ in range(population_count)]
 
 simulation_round = 1
 while simulation_round <= max_generation_iterations and canvas.is_simulating:
-    canvas.simulate_generation(networks, simulation_round)
     print(f"=== Round: {simulation_round} ===")
+    canvas.simulate_generation(networks, simulation_round)
     simulation_round += 1
     if canvas.is_simulating:
         print(f"-- Average checkpoint reached: {sum(n.highest_checkpoint for n in networks) / len(networks):.2f}.")
